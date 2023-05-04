f8b47ef260330208a8fed42bea3c836bd5e35bd2
Arjun M Menon <arjun@techgentsia.com>
2023-05-04 18:58:00 +0530

    added config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,11 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-
+    runtimeConfig: {
+    // The private keys which are only available within server-side
+    apiSecret: '123',
+    // Keys within public, will be also exposed to the client-side
+    public: {
+      apiBase: '/api'
+    }
+  }
 })
